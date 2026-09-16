c24e0fc9cdf32a765890aac36194e328e35e8870
Tanuja <tanujakalidindi5@gmail.com>
2026-09-16 08:29:35 +0530

    Time: 0 ms (100%), Space: 9.1 MB (12.48%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans,temp;
+        temp=s;
+        stack<char> st;
+        int i=0, j=0;
+        while(i<s.length()&&j<s.length()){
+            if(s[j]=='('||s[j]=='['||s[j]=='{')
+            st.push(s[j]);
+            else{
+                st.pop();
+            }
+            if(st.empty()){
+                for(int k=i+1;k<=j-1;k++)
+                ans.push_back(s[k]);
+                j++;
+                i=j;
+            }
+            else{
+                j++;
+            }
+        }
+        return ans;
+
+    }
+};
